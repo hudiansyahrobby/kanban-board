77b8a27a275211620aa1f7cc7094244a0a66d8b7
hudiansyahrobby <>
2024-04-20 12:33:18 +0800

    feat: add new asset icons

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,5 +1,19 @@
 import CloseIcon from "@/assets/icons/close-icon.svg?react";
 import CheckListIcon from "@/assets/icons/checklist-icon.svg?react";
 import ThreeDotsHorizontalIcon from "@/assets/icons/three-dots-horizontal-icon.svg?react";
+import ArrowLeftIcon from "@/assets/icons/arrow-left-icon.svg?react";
+import ArrowRightIcon from "@/assets/icons/arrow-right-icon.svg?react";
+import ExclamationIcon from "@/assets/icons/exclamation-icon.svg?react";
+import PlusCircleIcon from "@/assets/icons/plus-circle-icon.svg?react";
+import TrashIcon from "@/assets/icons/trash-icon.svg?react";
 
-export { CloseIcon, CheckListIcon, ThreeDotsHorizontalIcon };
+export {
+  CloseIcon,
+  CheckListIcon,
+  ThreeDotsHorizontalIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  ExclamationIcon,
+  PlusCircleIcon,
+  TrashIcon,
+};
