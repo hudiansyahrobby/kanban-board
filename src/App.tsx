678b41d3aa91b433dc80648e1be5b588d878825b
hudiansyahrobby <>
2024-04-20 11:48:30 +0800

    feat: add popover component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
+import Button from "./components/Button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./components/Dialog";
+import { Popover, PopoverContent, PopoverTrigger } from "./components/Popover";
+import ProgressBar from "./components/ProgressBar";
+
 function App() {
   return (
-    <h1 className="text-4xl font-bold underline text-primary-100">
-      Hello world!
-    </h1>
+    <div className="p-10">
+      <Popover>
+        <PopoverTrigger>Open</PopoverTrigger>
+        <PopoverContent side="bottom" sideOffset={10} align="start">
+          Place content for the popover here.
+        </PopoverContent>
+      </Popover>
+    </div>
   );
 }
 
--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,4 +1,5 @@
 import CloseIcon from "@/assets/icons/close-icon.svg?react";
 import CheckListIcon from "@/assets/icons/checklist-icon.svg?react";
+import ThreeDotsHorizontalIcon from "@/assets/icons/three-dots-horizontal-icon.svg?react";
 
-export { CloseIcon, CheckListIcon };
+export { CloseIcon, CheckListIcon, ThreeDotsHorizontalIcon };
