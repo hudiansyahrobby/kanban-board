8e3fa491e7f2e3ded8eb37919718baacaff7ece0
hudiansyahrobby <>
2024-04-20 09:23:06 +0800

    style: improve textInput component style on focus

--- src/components/Inputs/TextInput.tsx
+++ src/components/Inputs/TextInput.tsx
@@ -6,12 +6,14 @@ export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const TextInput = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, ...props }, ref) => {
     return (
       <input
-        type={type}
         className={cn(
-          "border-2 h-9 border-black-300 py-2 px-4 rounded-lg focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          "border-2 h-9 border-black-300 py-2 px-4 rounded-lg disabled:cursor-not-allowed disabled:opacity-50",
+          props.value
+            ? "focus-visible:outline-primary-100"
+            : "focus-visible:outline-primary-100/20",
           className
         )}
         ref={ref}
