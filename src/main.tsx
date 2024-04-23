d41cabf888ef7928252faa14e9101d0f2b62d25a
hudiansyahrobby <>
2024-04-23 09:24:27 +0800

    feat: add basename v1

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
-import { TodoProvider } from "./contexts/TodoContext.tsx";
+
+import { TodoProvider } from "@/contexts/TodoContext.tsx";
+import { router } from "@/routes/index.tsx";
+
+import "./index.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,7 +21,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <TodoProvider>
-        <App />
+        <RouterProvider router={router} />
       </TodoProvider>
       <Toaster position="top-center" richColors closeButton expand />
     </QueryClientProvider>
